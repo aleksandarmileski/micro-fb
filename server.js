506c82bd6e801e97ade5f881fb6f200e8e5c82da
aleksandarmileski <aleksandar.mileski00@gmail.com>
2018-11-14 23:02:30 +0100

    Added heroku setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const forceSSL = function () {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+app.use(forceSSL());
+
+app.use(express.static(__dirname + '/dist/micro-facebook/'));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/micro-facebook/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
