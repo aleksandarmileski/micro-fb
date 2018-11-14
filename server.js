0f2d6125e9e947d8a1518dffea693773280abaf7
aleksandarmileski <aleksandar.mileski00@gmail.com>
2018-11-15 00:58:04 +0100

    Added https

--- server.js
+++ server.js
@@ -6,10 +6,22 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/micro-facebook'));
 
+const forceSSL = function () {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+app.use(forceSSL());
+
 app.get('/*', function (req, res) {
 
     res.sendFile(path.join(__dirname + '/dist/micro-facebook/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080,()=>console.log('server listening ...'));
+app.listen(process.env.PORT || 8080, () => console.log('server listening ...'));
