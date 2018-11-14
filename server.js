8f12e43198fc6b142d80565e3621eb0882436908
aleksandarmileski <aleksandar.mileski00@gmail.com>
2018-11-14 23:12:58 +0100

    Heroku static file path change

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const forceSSL = function () {
 }
 app.use(forceSSL());
 
-app.use(express.static(__dirname + '/dist/micro-facebook/'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/micro-facebook/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
