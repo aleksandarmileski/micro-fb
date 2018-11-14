7b720c7517362b6b1d5cf71c2620f62e2450f025
aleksandarmileski <aleksandar.mileski00@gmail.com>
2018-11-14 23:28:48 +0100

    Heroku setup changes

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
 const express = require('express');
-const app = express();
-
 const path = require('path');
 
-const forceSSL = function () {
-    return function (req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
-            return res.redirect(
-                ['https://', req.get('Host'), req.url].join('')
-            );
-        }
-        next();
-    }
-}
-app.use(forceSSL());
+const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/micro-facebook'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+
+    res.sendFile(path.join(__dirname + '/dist/micro-facebook/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
